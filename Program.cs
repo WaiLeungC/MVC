d85d30f1032cbd3b1e3a8b07da388378c15d5165
Wai Leung Chan <chan-wai-leung@hotmail.com>
2025-01-30 15:41:35 +0100

    Setup Entity Framework Core

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
 
 // Initialize instance of the web application builder class which sets up the configurations, services and the webserver.
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add MVC-services to the dependency injection container with support for controllers and views.
 // Allows the application for handling incoming HTTP requests and renders HTML views.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 // Compile the app, creating a web application instance which can be configured and ran.
 var app = builder.Build();
