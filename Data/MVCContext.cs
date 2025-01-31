e9836055c1012e6ce5dc8de0f15c778bd12dac42
Wai Leung Chan <chan-wai-leung@hotmail.com>
2025-01-31 12:06:13 +0100

    Add One to One Relationships

--- Data/MVCContext.cs
+++ Data/MVCContext.cs
@@ -7,6 +7,20 @@ namespace MVC.Data
     {
         public MVCContext(DbContextOptions<MVCContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasData(
+                new Item { Id = 4, Name = "Microphone", Price = 10.0, SerialNumberId = 10 },
+                new Item { Id = 6, Name = "Keyboard", Price = 20.0, SerialNumberId = 13 }
+                );
+
+            modelBuilder.Entity<SerialNumber>().HasData(
+                new SerialNumber { Id = 10, Name = "MIC150", ItemId = 4 },
+                new SerialNumber { Id = 13, Name = "TKL10", ItemId = 6 }
+                );
+        }
+
         public DbSet<Item> Items { get; set; }
+        public DbSet<SerialNumber> SerialNumbers { get; set; }
     }
 }
